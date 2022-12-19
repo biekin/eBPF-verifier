2d6f107cc546a9d2e8d6b5be9b09f57bf28c4c5d
biekin <26335041+biekin@users.noreply.github.com>
2022-12-19 22:56:12 +0100

    Add project structore and Interval, IExpression, IProgramVariable

---
+++ eBPF-verifier/Interval.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace eBPF_verifier
+{
+	public class Interval
+	{
+		public int From { get; private set; }
+		public int To { get; private set; }
+
+		public Interval(int from, int to)
+		{
+			From = from;
+			To = to;
+		}
+
+		public Interval Add(Interval another)
+		{
+			return new Interval(From + another.From, To + another.To);
+		}
+
+        public Interval Subtract(Interval another)
+        {
+            return new Interval(From - another.From, To - another.To);
+        }
+    }
+}
+
