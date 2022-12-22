e966b547490ec18230c93cf5e11772f0da4eee25
biekin <26335041+biekin@users.noreply.github.com>
2022-12-22 07:30:16 +0100

    Update Equation

---
+++ eBPF-verifier/Equation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace eBPF_verifier
+{
+	public class Equation
+	{
+		private ProgramPoint ProgramPoint;
+
+		private AbstractExpression AbstractExpression;
+
+		private AbstractState AbstractState;
+
+		public Equation(ProgramPoint programPoint, AbstractExpression abstractExpression, AbstractState abstractState)
+		{
+			ProgramPoint = programPoint;
+			AbstractExpression = abstractExpression;
+			AbstractState = abstractState;
+		}
+	}
+}
+
