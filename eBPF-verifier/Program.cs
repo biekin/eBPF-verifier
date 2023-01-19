f35bcf05bd7200262726fd45a0fd76ae37e2b824
biekin <26335041+biekin@users.noreply.github.com>
2023-01-19 05:04:01 +0100

    Add tests and further work on iterators

--- eBPF-verifier/Program.cs
+++ eBPF-verifier/Program.cs
@@ -1,13 +1,19 @@
 ﻿using eBPF_verifier;
 
-//var ex1 = new Example1();
-//ex1.Execute();
+public class Program
+{
+    public static void Main()
+    {
+        //var ex1 = new Example1();
+        //ex1.Execute();
 
-//var ex2 = new Example2();
-//ex2.Execute();
+        //var ex2 = new Example2();
+        //ex2.Execute();
 
-//var ex3 = new Example3();
-//ex3.Execute();
+        //var ex3 = new Example3();
+        //ex3.Execute();
+    }
+}
 
 
 
---
+++ eBPF-verifierTests/AnalyzerTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eBPF_verifier;
+namespace eBPF_verifierTests;
+
+[TestClass]
+public class AnalyzerTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
