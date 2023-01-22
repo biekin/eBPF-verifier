d0b94910fc332acf59e75a8bb910fa95a8fadb47
biekin <26335041+biekin@users.noreply.github.com>
2023-01-22 02:05:02 +0100

    Refactor

--- eBPF-verifier/Program.cs
+++ eBPF-verifier/Program.cs
@@ -7,15 +7,14 @@ public class Program
         //var ex1 = new Example1();
         //ex1.Execute();
 
-        //var ex2 = new Example2();
-        //ex2.Execute();
+        var ex2 = new Example2();
+        ex2.Execute();
 
         //var ex3 = new Example3();
         //ex3.Execute();
 
         //var ex4 = new Example4();
         //ex4.Execute();
-        Console.WriteLine(-5 % (-3));
     }
 }
 
